9204e1c6a67630b246af8d4a9b57a843281fc714
rguixaro <rguixaro@protonmail.ch>
2024-10-31 20:31:15 +0100

    feat(config): update config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,53 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-const config: Config = {
+const config = {
+	darkMode: ['class'],
 	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
 	],
+	prefix: '',
 	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: { '2xl': '1400px' },
+		},
 		extend: {
-			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)',
-				'green-forest': '#205550',
+			colors: { 'green-forest': '#205550' },
+			fontFamily: {
+				sans: ['var(--font-atkinson-regular)', ...fontFamily.sans],
+				mono: ['var(--font-atkinson-bold)', ...fontFamily.mono],
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' },
+				},
+				'background-shine': {
+					from: {
+						backgroundPosition: '0 0',
+					},
+					to: {
+						backgroundPosition: '-200% 0',
+					},
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'background-shine': 'background-shine 2s linear infinite',
 			},
 		},
 	},
-	plugins: [],
-};
+	plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+} satisfies Config;
+
 export default config;
