ac060f82a9569915ddeff56c7d6c995fa9a8ea2f
rguixaro <rguixaro@protonmail.ch>
2024-11-12 23:36:02 +0100

    feat(utils): add messages constants

---
+++ src/utils/messages.ts
@@ -0,0 +1,9 @@
+export const MESSAGES = {
+	ERROR: 'An unexpected error has occurred. Please try again later.',
+	LINK_LIMIT_REACHED: 'You have reached the limit of links you can create.',
+	LINK_CREATED: 'Link created successfully',
+	LINK_EXISTS:
+		'This link already exists. Try another or use the randomize option.',
+	SAME_SLUG_URL: 'The URL and the slug cannot be the same',
+	AUTH_ERROR: 'Authentication failed',
+};
