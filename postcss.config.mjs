146a40b10619d52ab5b67e7a2fe950cfbdd35172
rguixaro <developer@rguixaro.dev>
2025-09-27 11:58:23 +0200

    feat(style): upgrade to tailwind v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
+export default {
+	plugins: {
+		'@tailwindcss/postcss': {},
+		autoprefixer: {},
+	},
+}
